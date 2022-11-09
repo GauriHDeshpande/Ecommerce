713d707444acdfcb70c6100fd0a0114c7d9752cc
Gauri Deshpande <gauri.deshpande22@rediffmail.com>
2022-11-09 20:42:20 +0530

    started Ecommerse Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <h2>Welcome to React Ecommerse App!!</h2>
+    </div>
+  );
+}
+
+export default App;
